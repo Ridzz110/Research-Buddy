0be237e73cb17216cd3997f3701afd0b9897f0c7
Ridzz110 <84570922+Ridzz110@users.noreply.github.com>
2026-02-28 18:35:10 +0500

    removing copy to clipboard button from markdown

--- app.py
+++ app.py
@@ -72,7 +72,6 @@ with gr.Blocks(title="Research Buddy") as app:
         sources_output = gr.Textbox(label="Sources", lines=5)
         report_output = gr.Markdown(
             label="Report :",
-            buttons=['copy']
         )
 
     submit_btn.click(
